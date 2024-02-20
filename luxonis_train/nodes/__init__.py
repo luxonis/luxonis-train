7c16e4739989b4b4ec196716e89c01601d5c819b
Drejc Pesjak <47791324+DrejcPesjak@users.noreply.github.com>
2024-02-20 03:44:01 +0100

    Resnet Variants (#9)
    * Added ResNet variants
    
    * ResNet50 example
    
    * Simplified example
    
    * fixed resnet config
    
    * [Automated] Updated coverage badge
    
    ---------
    
    Co-authored-by: Martin Kozlovsky <martin.kozlovsky@luxonis.com>
    Co-authored-by: GitHub Actions <actions@github.com>

--- luxonis_train/nodes/__init__.py
+++ luxonis_train/nodes/__init__.py
@@ -10,7 +10,7 @@ from .mobilenetv2 import MobileNetV2
 from .mobileone import MobileOne
 from .reppan_neck import RepPANNeck
 from .repvgg import RepVGG
-from .resnet18 import ResNet18
+from .resnet import ResNet
 from .rexnetv1 import ReXNetV1_lite
 from .segmentation_head import SegmentationHead
 
@@ -28,6 +28,6 @@ __all__ = [
     "ReXNetV1_lite",
     "RepPANNeck",
     "RepVGG",
-    "ResNet18",
+    "ResNet",
     "SegmentationHead",
 ]
