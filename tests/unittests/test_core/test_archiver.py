5e59c3aebb8a17543446b144697f2e47bc0b67af
jkbmrz <jakob.mraz@gmail.com>
2024-03-14 15:56:23 +0100

    base archiver tests on model from luxonis-train instead of torchvision

--- tests/unittests/test_core/test_archiver.py
+++ tests/unittests/test_core/test_archiver.py
@@ -6,15 +6,17 @@ import shutil
 import tarfile
 
 import cv2
+import lightning.pytorch as pl
 import numpy as np
 import onnx
-import torch
-import torchvision
 import yaml
 from luxonis_ml.data import LuxonisDataset
 
 import luxonis_train
 from luxonis_train.core import Archiver
+from luxonis_train.core.exporter import Exporter
+from luxonis_train.core.trainer import Trainer
+from luxonis_train.utils.config import Config
 
 
 class TestArchiver:
@@ -26,11 +28,9 @@ class TestArchiver:
             os.path.dirname(luxonis_train.__file__)
         )
         cls.tmp_path = os.path.join(
-            luxonis_train_parent_dir,
-            "tests",
-            "unittests",
-            "test_core",
+            luxonis_train_parent_dir, "tests", "unittests", "test_core", "tmp"
         )
+        os.mkdir(cls.tmp_path)
 
         # make LDF
         os.mkdir(os.path.join(cls.tmp_path, "images"))
@@ -39,7 +39,7 @@ class TestArchiver:
 
         def classification_dataset_generator():
             for i in range(10):
-                img = np.random.randint(0, 256, (50, 50, 3), dtype=np.uint8)
+                img = np.random.randint(0, 256, (10, 10, 3), dtype=np.uint8)
                 img_file_path = os.path.join(cls.tmp_path, "images", f"img{i}.png")
                 cv2.imwrite(img_file_path, img)
                 yield {
@@ -64,36 +64,37 @@ class TestArchiver:
                 "predefined_model": {"name": "ClassificationModel"},
             },
             "dataset": {"name": cls.ldf_name},
-            "archiver": {
-                "archive_name": "tmp_nn_archive",
-                "archive_save_directory": cls.tmp_path,
-            },
+            "tracker": {"save_directory": cls.tmp_path},
         }
-        cls.config_path = os.path.join(cls.tmp_path, "tmp_config.yaml")
+        cls.config_path = os.path.join(cls.tmp_path, "config.yaml")
         with open(cls.config_path, "w") as yaml_file:
             yaml_str = yaml.dump(config_dict)
             yaml_file.write(yaml_str)
+        cfg = Config.get_config(config_dict)
 
-        # make model
-        model = torchvision.models.mobilenet_v2(pretrained=False)
-        cls.model_path = os.path.join(cls.tmp_path, "tmp_mobilenet_v2.onnx")
+        # train model
+        cfg.trainer.epochs = 1
+        cfg.trainer.validation_interval = 1
+        cfg.trainer.batch_size = 4
+        trainer = Trainer(cfg=cfg)
+        trainer.train()
+        callbacks = [
+            c
+            for c in trainer.pl_trainer.callbacks
+            if isinstance(c, pl.callbacks.ModelCheckpoint)
+        ]
+        model_checkpoint_path = callbacks[0].best_model_path
+        model_ckpt = os.path.join(trainer.run_save_dir, model_checkpoint_path)
 
-        n, c, h, w = 1, 3, 224, 224
-        input_shape = torch.randn(n, c, h, w)
-        cls.input_names = ["TestInput"]
-        cls.output_names = ["TestOutput"]
-
-        torch.onnx.export(
-            model,
-            input_shape,
-            cls.model_path,
-            verbose=False,
-            input_names=cls.input_names,
-            output_names=cls.output_names,
-        )
+        # export model to ONNX
+        cfg.model.weights = model_ckpt
+        exporter = Exporter(cfg=cfg)
+        cls.onnx_model_path = os.path.join(cls.tmp_path, "model.onnx")
+        exporter.export(onnx_path=cls.onnx_model_path)
 
         # make archive
-        cls.archive_path = Archiver(cls.config_path).archive(cls.model_path)
+        cfg.archiver.archive_save_directory = cls.tmp_path
+        cls.archive_path = Archiver(cls.config_path).archive(cls.onnx_model_path)
 
         # load archive files into memory
         with tarfile.open(cls.archive_path, mode="r") as tar:
@@ -110,11 +111,8 @@ class TestArchiver:
     @classmethod
     def teardown_class(cls):
         """Remove all created files."""
-        os.remove(cls.archive_path)
-        os.remove(cls.config_path)
-        os.remove(cls.model_path)
         LuxonisDataset(cls.ldf_name).delete_dataset()
-        shutil.rmtree(os.path.join(cls.tmp_path, "images"))
+        shutil.rmtree(cls.tmp_path)
 
     def test_archive_creation(self):
         """Test if nn_archive was created."""
@@ -126,12 +124,11 @@ class TestArchiver:
         assert self.archive_path.endswith("tar.xz")
 
     def test_archive_contents(self):
-        """Test if nn_archive consists of exactly one JSON file named config.json and
-        one ONNX file."""
+        """Test if nn_archive consists of config.json and model.onnx."""
         assert (
             len(self.archive_fnames) == 2
             and any([fname == "config.json" for fname in self.archive_fnames])
-            and any([fname.endswith(".onnx") for fname in self.archive_fnames])
+            and any([fname == "model.onnx" for fname in self.archive_fnames])
         )
 
     def test_onnx(self):
@@ -143,11 +140,15 @@ class TestArchiver:
         config_input_names = []
         for input in self.json_dict["model"]["inputs"]:
             config_input_names.append(input["name"])
-        assert set(self.input_names) == set(config_input_names)
+        assert set([input.name for input in self.onnx_model.graph.input]) == set(
+            config_input_names
+        )
 
     def test_config_outputs(self):
         """Test if archived config outputs are valid."""
         config_output_names = []
         for input in self.json_dict["model"]["outputs"]:
             config_output_names.append(input["name"])
-        assert set(self.output_names) == set(config_output_names)
+        assert set([output.name for output in self.onnx_model.graph.output]) == set(
+            config_output_names
+        )
