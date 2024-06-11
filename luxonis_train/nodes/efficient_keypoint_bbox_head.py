8a31223ae734c2eee8527773382f2143efa7fa32
Jernej Sabadin <jernej14sabadin@gmail.com>
2024-06-11 11:07:58 +0200

    Proper export

--- luxonis_train/nodes/efficient_keypoint_bbox_head.py
+++ luxonis_train/nodes/efficient_keypoint_bbox_head.py
@@ -105,17 +105,18 @@ class EfficientKeypointBBoxHead(EfficientBBoxHead):
             for out_cls, out_reg, out_kpts in zip(
                 cls_score_list, reg_distri_list, kpt_list, strict=True
             ):
-                out = torch.cat([out_reg, out_cls, out_kpts], dim=1)
+                chunks = out_kpts.split(3, dim=1)
+                modified_chunks = []
+                for chunk in chunks:
+                    x = chunk[:, 0:1, :, :]
+                    y = chunk[:, 1:2, :, :]
+                    v = torch.sigmoid(chunk[:, 2:3, :, :])
+                    modified_chunk = torch.cat([x, y, v], dim=1)
+                    modified_chunks.append(modified_chunk)
+                out_kpts_modified = torch.cat(modified_chunks, dim=1)
+                out = torch.cat([out_reg, out_cls, out_kpts_modified], dim=1)
                 outputs.append(out)
-            kpt_tensor = torch.cat(
-                [
-                    kpt_list[i].view(bs, self.nk, -1).flatten(2)
-                    for i in range(len(kpt_list))
-                ],
-                dim=2,
-            ).permute(0, 2, 1)
-            pred_kpt = self._kpts_decode(kpt_tensor)
-            return {"outputs": outputs, "kpts": [pred_kpt]}
+            return {"outputs": outputs}
         cls_tensor = torch.cat(
             [cls_score_list[i].flatten(2) for i in range(len(cls_score_list))], dim=2
         ).permute(0, 2, 1)
@@ -165,26 +166,9 @@ class EfficientKeypointBBoxHead(EfficientBBoxHead):
         anchor_points_x = anchor_points_transposed[0].view(1, -1, 1)
         anchor_points_y = anchor_points_transposed[1].view(1, -1, 1)
 
-        # Using torch functions to achieve the desired indexing
-        idx_0 = torch.arange(0, y.size(2), 3, device=y.device)
-        idx_1 = torch.arange(1, y.size(2), 3, device=y.device)
-        idx_2 = torch.arange(2, y.size(2), 3, device=y.device)
-
-        y.index_add_(
-            2,
-            idx_0,
-            (y.index_select(2, idx_0) * 2.0 + (anchor_points_x - 0.5)) * stride_tensor
-            - y.index_select(2, idx_0),
-        )
-        y.index_add_(
-            2,
-            idx_1,
-            (y.index_select(2, idx_1) * 2.0 + (anchor_points_y - 0.5)) * stride_tensor
-            - y.index_select(2, idx_1),
-        )
-        y.index_add_(
-            2, idx_2, y.index_select(2, idx_2).sigmoid() - y.index_select(2, idx_2)
-        )
+        y[:, :, 0::3] = (y[:, :, 0::3] * 2.0 + (anchor_points_x - 0.5)) * stride_tensor
+        y[:, :, 1::3] = (y[:, :, 1::3] * 2.0 + (anchor_points_y - 0.5)) * stride_tensor
+        y[:, :, 2::3] = y[:, :, 2::3].sigmoid()
 
         return y
 
