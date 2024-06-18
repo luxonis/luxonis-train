06eb7779f9d7a771520c949b0b0e4e0bc57f9ebd
MatejRojec <matej.4001@gmail.com>
2024-06-18 18:51:39 +0200

    possible mobilnet fix

--- luxonis_train/nodes/mobilenetv2.py
+++ luxonis_train/nodes/mobilenetv2.py
@@ -30,9 +30,8 @@ class MobileNetV2(BaseNode[Tensor, list[Tensor]]):
             weights="DEFAULT" if download_weights else None
         )
         mobilenet_v2.classifier = nn.Identity()
-        mobilenet_v2.features[18] = nn.Identity()
 
-        self.out_indices = [3, 6, 13, 17]
+        self.out_indices = [3, 6, 13, 17, 18]
         self.channels = [24, 32, 96, 320]
         self.backbone = mobilenet_v2
 
