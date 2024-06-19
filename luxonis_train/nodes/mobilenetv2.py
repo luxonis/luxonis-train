ee25f5dae37595a36bd3ebb86686b2d92f25da0d
MatejRojec <matej.4001@gmail.com>
2024-06-19 11:46:16 +0200

    mobilnet change chanels

--- luxonis_train/nodes/mobilenetv2.py
+++ luxonis_train/nodes/mobilenetv2.py
@@ -30,10 +30,9 @@ class MobileNetV2(BaseNode[Tensor, list[Tensor]]):
             weights="DEFAULT" if download_weights else None
         )
         mobilenet_v2.classifier = nn.Identity()
-        # mobilenet_v2.features[18] = nn.Identity()
 
         self.out_indices = [3, 6, 13, 18]
-        self.channels = [24, 32, 96, 320]
+        self.channels = [24, 32, 96, 1280]
         self.backbone = mobilenet_v2
 
 
