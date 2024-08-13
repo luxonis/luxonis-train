67a6c089fd26b761b6d91420195abe56a6774338
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-08-14 00:13:59 +0200

    optional auto-finalize

--- luxonis_train/utils/tracker.py
+++ luxonis_train/utils/tracker.py
@@ -1,5 +1,3 @@
-from typing import Literal
-
 from lightning.pytorch.loggers.logger import Logger
 from lightning.pytorch.utilities import rank_zero_only  # type: ignore
 from luxonis_ml.tracker import LuxonisTracker
@@ -8,21 +6,31 @@ from luxonis_ml.tracker import LuxonisTracker
 class LuxonisTrackerPL(LuxonisTracker, Logger):
     """Implementation of LuxonisTracker that is compatible with PytorchLightning."""
 
+    def __init__(self, *, _auto_finalize: bool = True, **kwargs):
+        """
+        @type _auto_finalize: bool
+        @param _auto_finalize: If True, the run will be finalized automatically when the training ends.
+            If set to C{False}, the user will have to call the L{_finalize} method manually.
+
+        @type kwargs: dict
+        @param kwargs: Additional keyword arguments to be passed to the L{LuxonisTracker}.
+        """
+        LuxonisTracker.__init__(self, **kwargs)
+        Logger.__init__(self)
+        if _auto_finalize:
+            self.finalize = self._finalize
+
     @rank_zero_only
-    def finalize(
-        self, status: Literal["success", "failed", "finished"] = "success"
-    ) -> None:
+    def _finalize(self, status: str = "success") -> None:
         """Finalizes current run."""
         if self.is_tensorboard:
             self.experiment["tensorboard"].flush()
             self.experiment["tensorboard"].close()
         if self.is_mlflow:
-            if status == "success":
+            if status in ["success", "finished"]:
                 mlflow_status = "FINISHED"
-            elif status == "failed":
+            else:
                 mlflow_status = "FAILED"
-            elif status == "finished":
-                mlflow_status = "FINISHED"
             self.experiment["mlflow"].end_run(mlflow_status)
         if self.is_wandb:
             if status == "success":
