a94c1d650190b913f1d714470ceb52b372437979
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-09-09 07:59:10 +0200

    removed multiclass from focal loss until it is fixed

--- luxonis_train/attached_modules/losses/softmax_focal_loss.py
+++ luxonis_train/attached_modules/losses/softmax_focal_loss.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Any, Literal
 
 import torch
@@ -8,14 +9,16 @@ from luxonis_train.attached_modules.losses import BaseLoss
 
 from .cross_entropy import CrossEntropyLoss
 
+logger = logging.getLogger(__name__)
 
-# TODO: Make focal losses support multi-class tasks
+
+# TODO: Add support for multi-class tasks
 class SoftmaxFocalLoss(BaseLoss[Tensor, Tensor]):
     supported_labels = [LabelType.SEGMENTATION, LabelType.CLASSIFICATION]
 
     def __init__(
         self,
-        alpha: float | list[float] = 0.25,
+        alpha: float = 0.25,
         gamma: float = 2.0,
         reduction: Literal["none", "mean", "sum"] = "mean",
         **kwargs: Any,
@@ -23,7 +26,7 @@ class SoftmaxFocalLoss(BaseLoss[Tensor, Tensor]):
         """Focal loss implementation for binary classification and segmentation tasks
         using Softmax.
 
-        @type alpha: float | list[float]
+        @type alpha: float
         @param alpha: Weighting factor for the rare class. Defaults to C{0.25}.
         @type gamma: float
         @param gamma: Focusing parameter. Defaults to C{2.0}.
@@ -40,13 +43,7 @@ class SoftmaxFocalLoss(BaseLoss[Tensor, Tensor]):
     def forward(self, predictions: Tensor, target: Tensor) -> Tensor:
         ce_loss = self.ce_criterion.forward(predictions, target)
         pt = torch.exp(-ce_loss)
-        loss = ce_loss * ((1 - pt) ** self.gamma)
-
-        if isinstance(self.alpha, float) and self.alpha >= 0:
-            loss = self.alpha * loss
-        elif isinstance(self.alpha, list):
-            alpha_t = torch.tensor(self.alpha)[target]
-            loss = alpha_t * loss
+        loss = ce_loss * ((1 - pt) ** self.gamma) * self.alpha
 
         if self.reduction == "mean":
             loss = loss.mean()
