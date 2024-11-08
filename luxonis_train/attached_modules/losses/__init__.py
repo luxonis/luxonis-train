cc791275a19b741c205c972b51fdbd18f1b90461
Jernej Sabadin <jernej14sabadin@gmail.com>
2024-11-08 09:53:01 +0100

    precommit hooks

--- luxonis_train/attached_modules/losses/__init__.py
+++ luxonis_train/attached_modules/losses/__init__.py
@@ -3,10 +3,10 @@ from .base_loss import BaseLoss
 from .bce_with_logits import BCEWithLogitsLoss
 from .cross_entropy import CrossEntropyLoss
 from .efficient_keypoint_bbox_loss import EfficientKeypointBBoxLoss
-from .reconstruction_segmentation_loss import ReconstructionSegmentationLoss
 from .ohem_bce_with_logits import OHEMBCEWithLogitsLoss
 from .ohem_cross_entropy import OHEMCrossEntropyLoss
 from .ohem_loss import OHEMLoss
+from .reconstruction_segmentation_loss import ReconstructionSegmentationLoss
 from .sigmoid_focal_loss import SigmoidFocalLoss
 from .smooth_bce_with_logits import SmoothBCEWithLogitsLoss
 from .softmax_focal_loss import SoftmaxFocalLoss
