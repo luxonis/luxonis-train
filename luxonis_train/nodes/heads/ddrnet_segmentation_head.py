13663ed5d7cd6693728ce7dc1e231bdcef306b7f
HonzaCuhel <jan.cuhel@protonmail.com>
2024-11-18 14:48:10 +0100

    Fix binary segmentation export

--- luxonis_train/nodes/heads/ddrnet_segmentation_head.py
+++ luxonis_train/nodes/heads/ddrnet_segmentation_head.py
@@ -105,5 +105,6 @@ class DDRNetSegmentationHead(BaseNode[Tensor, Tensor]):
         x = self.conv2(x)
         x = self.upscale(x)
         if self.export:
-            return x.argmax(dim=1).to(dtype=torch.int32)
+            x = x.argmax(dim=1) if self.n_classes > 1 else (x > 0)
+            return x.to(dtype=torch.int32)
         return x
