75719353e757c2f3df43b7a70164cbaf2b6eb782
klemen1999 <klemenskrlj8@gmail.com>
2024-12-09 16:38:53 +0100

    seg model weights path update

--- luxonis_train/nodes/backbones/ddrnet/variants.py
+++ luxonis_train/nodes/backbones/ddrnet/variants.py
@@ -19,7 +19,7 @@ def get_variant(variant: Literal["23-slim", "23"]) -> DDRNetVariant:
         "23": DDRNetVariant(
             channels=64,
             highres_channels=128,
-            weights_path=None,
+            weights_path="https://github.com/luxonis/luxonis-train/releases/download/v0.2.1-beta/ddrnet_23_coco.ckpt",
         ),
     }
     if variant not in variants:  # pragma: no cover
