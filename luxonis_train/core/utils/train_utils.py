105a57efa099ed8bfb1a0c7b97bcdc55a35a396b
Jernej Sabadin <jernej14sabadin@gmail.com>
2024-12-13 15:29:33 +0100

    test

--- luxonis_train/core/utils/train_utils.py
+++ luxonis_train/core/utils/train_utils.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any
 
 import lightning.pytorch as pl
@@ -16,11 +17,32 @@ def create_trainer(cfg: TrainerConfig, **kwargs: Any) -> pl.Trainer:
     @rtype: pl.Trainer
     @return: Pytorch Lightning trainer.
     """
-    strategy = (  # WARNING: DDP training fix
+    os.environ["NCCL_DEBUG"] = "INFO"
+    os.environ["NCCL_SOCKET_IFNAME"] = (
+        "eth0"  # Adjust based on your network interface
+    )
+    os.environ["NCCL_IB_DISABLE"] = "1"  # Disable InfiniBand if not available
+
+    strategy = (  # WARNING: DDP training FREEZES when using LightningEnvironment!!!
         DDPStrategy(cluster_environment=LightningEnvironment())
         if cfg.strategy.lower() == "ddp"
         else cfg.strategy
     )
+    # printenv | grep -E "WORLD_SIZE|RANK|LOCAL_RANK|MASTER_ADDR|MASTER_PORT"
+
+    # output:
+    # root@detection-zz22crkgtlnxzc-luxonistrain-0-0:/workspace# printenv | grep -E "WORLD_SIZE|RANK|LOCAL_RANK|MASTER_ADDR|MASTER_PORT"
+    # TORCHX_RANK0_HOST=localhost
+    # WORLD_SIZE=2
+
+    # MANUALY EXPORT:
+    # export NCCL_DEBUG=INFO
+    # export NCCL_SOCKET_IFNAME=eth0  # Use the correct network interface
+    # export NCCL_IB_DISABLE=1  # Disable InfiniBand if not available
+
+    # START TRAINGING luxonis_trian train --config TEST_GCP.yaml
+    # Manualy trained -> WORKED!!!
+
     return pl.Trainer(
         accelerator=cfg.accelerator,
         devices=cfg.devices,
