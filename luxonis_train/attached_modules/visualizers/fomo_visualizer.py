fd24193e667b5b92171cb65ca9cfed1f2a972243
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-01-27 09:11:35 -0500

    added fomo visualizer

---
+++ luxonis_train/attached_modules/visualizers/fomo_visualizer.py
@@ -0,0 +1,26 @@
+from torch import Tensor
+
+from luxonis_train.attached_modules.visualizers import BBoxVisualizer
+from luxonis_train.enums import Task
+
+from .keypoint_visualizer import KeypointVisualizer
+
+
+class FOMOVisualizer(BBoxVisualizer):
+    supported_tasks = [Task.FOMO]
+
+    def forward(
+        self,
+        target_canvas: Tensor,
+        prediction_canvas: Tensor,
+        keypoints: list[Tensor],
+        target_boundingbox: Tensor | None,
+    ) -> tuple[Tensor, Tensor] | Tensor:
+        pred_viz = KeypointVisualizer.draw_predictions(
+            prediction_canvas, keypoints, colors="red", radius=5
+        )
+        if target_boundingbox is None:
+            return pred_viz
+
+        target_viz = super().draw_targets(target_canvas, target_boundingbox)
+        return target_viz, pred_viz
