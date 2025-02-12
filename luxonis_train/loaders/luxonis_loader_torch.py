a799da781f17d50bcd429f7dad19f71de608f4d8
Jernej Sabadin <jernej14sabadin@gmail.com>
2025-02-12 10:31:19 +0100

    dict of lists

--- luxonis_train/loaders/luxonis_loader_torch.py
+++ luxonis_train/loaders/luxonis_loader_torch.py
@@ -119,7 +119,12 @@ class LuxonisLoaderTorch(BaseLoaderTorch):
 
     @override
     def get_classes(self) -> dict[str, list[str]]:
-        return self.dataset.get_classes()
+        return {
+            task: [
+                cls for cls, _ in sorted(mapping.items(), key=lambda x: x[1])
+            ]
+            for task, mapping in self.dataset.get_classes().items()
+        }
 
     @override
     def get_n_keypoints(self) -> dict[str, int]:
