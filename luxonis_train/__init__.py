1ddc290d4fc32f49f981746b74dce6cea67bcf7a
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-03-01 03:05:25 +0100

    faster CLI

--- luxonis_train/__init__.py
+++ luxonis_train/__init__.py
@@ -1,27 +1,30 @@
 __version__ = "0.3.0"
 
+import shutil
+import sys
 import warnings
 
-try:
-    from .attached_modules import *
-    from .config import *
-    from .core import *
-    from .loaders import *
-    from .models import *
-    from .nodes import *
-    from .optimizers import *
-    from .schedulers import *
-    from .strategies import *
-    from .tasks import *
-    from .utils import *
-    from .utils import setup_logging
+if not sys.argv or sys.argv[0] != shutil.which("luxonis_train"):
+    try:
+        from .attached_modules import *
+        from .config import *
+        from .core import *
+        from .loaders import *
+        from .models import *
+        from .nodes import *
+        from .optimizers import *
+        from .schedulers import *
+        from .strategies import *
+        from .tasks import *
+        from .utils import *
+        from .utils import setup_logging
 
-    setup_logging()
+        setup_logging()
 
-except ImportError as e:
-    warnings.warn(
-        "Failed to import submodules. "
-        "Some functionality of `luxonis-train` may be unavailable. "
-        f"Error: `{e}`",
-        stacklevel=2,
-    )
+    except ImportError as e:
+        warnings.warn(
+            "Failed to import submodules. "
+            "Some functionality of `luxonis-train` may be unavailable. "
+            f"Error: `{e}`",
+            stacklevel=2,
+        )
