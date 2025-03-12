f4a5813769d986c96b3ea5a9675b195a5210bab2
Petros Toupas <ptoupas@gmail.com>
2025-03-12 17:11:14 +0200

    Add MIoU and DiceCoefficient metrics for semantic segmentation tasks

--- luxonis_train/attached_modules/metrics/__init__.py
+++ luxonis_train/attached_modules/metrics/__init__.py
@@ -1,7 +1,9 @@
 from .base_metric import BaseMetric
 from .confusion_matrix import ConfusionMatrix
+from .dice_coefficient import DiceCoefficient
 from .embedding_metrics import ClosestIsPositiveAccuracy, MedianDistances
 from .mean_average_precision import MeanAveragePrecision
+from .mean_iou import MIoU
 from .object_keypoint_similarity import ObjectKeypointSimilarity
 from .ocr_accuracy import OCRAccuracy
 from .torchmetrics import Accuracy, F1Score, JaccardIndex, Precision, Recall
@@ -19,4 +21,6 @@ __all__ = [
     "ConfusionMatrix",
     "MedianDistances",
     "OCRAccuracy",
+    "DiceCoefficient",
+    "MIoU",
 ]
