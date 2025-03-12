533e488dc5cf0574fda19418f0bff020d3b594f5
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-03-12 23:54:56 +0100

    split confusion matrix

---
+++ luxonis_train/attached_modules/metrics/confusion_matrix/__init__.py
@@ -0,0 +1,9 @@
+from .confusion_matrix import ConfusionMatrix
+from .detection_confusion_matrix import DetectionConfusionMatrix
+from .recognition_confusion_matrix import RecognitionConfusionMatrix
+
+__all__ = [
+    "ConfusionMatrix",
+    "DetectionConfusionMatrix",
+    "RecognitionConfusionMatrix",
+]
