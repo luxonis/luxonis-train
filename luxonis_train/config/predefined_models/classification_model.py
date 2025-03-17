ac0c853893a3795d56d67006151e666af062893d
Jernej Sabadin <116955183+JSabadin@users.noreply.github.com>
2025-03-17 17:45:12 +0100

    fix missing task names (#192)
    Co-authored-by: conorsim <bato.simmons@gmail.com>

--- luxonis_train/config/predefined_models/classification_model.py
+++ luxonis_train/config/predefined_models/classification_model.py
@@ -78,14 +78,12 @@ class ClassificationModel(BasePredefinedModel):
         return [
             ModelNodeConfig(
                 name=self.backbone,
-                alias=f"{self.task_name}-{self.backbone}",
                 freezing=self.backbone_params.pop("freezing", {}),
                 params=self.backbone_params,
             ),
             ModelNodeConfig(
                 name="ClassificationHead",
-                alias=f"{self.task_name}-ClassificationHead",
-                inputs=[f"{self.task_name}-{self.backbone}"],
+                inputs=[f"{self.backbone}"],
                 freezing=self.head_params.pop("freezing", {}),
                 params=self.head_params,
                 task_name=self.task_name,
@@ -98,7 +96,7 @@ class ClassificationModel(BasePredefinedModel):
         return [
             LossModuleConfig(
                 name="CrossEntropyLoss",
-                attached_to=f"{self.task_name}-ClassificationHead",
+                attached_to="ClassificationHead",
                 params=self.loss_params,
                 weight=1.0,
             )
@@ -111,17 +109,17 @@ class ClassificationModel(BasePredefinedModel):
             MetricModuleConfig(
                 name="F1Score",
                 is_main_metric=True,
-                attached_to=f"{self.task_name}-ClassificationHead",
+                attached_to="ClassificationHead",
                 params={"task": self.task},
             ),
             MetricModuleConfig(
                 name="Accuracy",
-                attached_to=f"{self.task_name}-ClassificationHead",
+                attached_to="ClassificationHead",
                 params={"task": self.task},
             ),
             MetricModuleConfig(
                 name="Recall",
-                attached_to=f"{self.task_name}-ClassificationHead",
+                attached_to="ClassificationHead",
                 params={"task": self.task},
             ),
         ]
@@ -129,8 +127,7 @@ class ClassificationModel(BasePredefinedModel):
             metrics.append(
                 MetricModuleConfig(
                     name="ConfusionMatrix",
-                    alias=f"{self.task_name}-ConfusionMatrix",
-                    attached_to=f"{self.task_name}-ClassificationHead",
+                    attached_to="ClassificationHead",
                     params={**self.confusion_matrix_params},
                 )
             )
@@ -142,7 +139,7 @@ class ClassificationModel(BasePredefinedModel):
         return [
             AttachedModuleConfig(
                 name="ClassificationVisualizer",
-                attached_to=f"{self.task_name}-ClassificationHead",
+                attached_to="ClassificationHead",
                 params=self.visualizer_params,
             )
         ]
