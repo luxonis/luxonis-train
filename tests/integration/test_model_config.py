d1d276563e74987af14e9a012a625270ca98e4c2
Jernej Sabadin <jernej14sabadin@gmail.com>
2025-04-16 17:40:12 +0200

    use .dict()

--- tests/integration/test_model_config.py
+++ tests/integration/test_model_config.py
@@ -6,17 +6,6 @@ from luxonis_train import LuxonisModel
 from luxonis_train.config.config import Config as LuxonisTrainConfig
 
 
-def normalize_dict(d):
-    """Convert any nested dictionaries to sorted items for
-    comparison."""
-    if isinstance(d, dict):
-        return {k: normalize_dict(v) for k, v in sorted(d.items())}
-    elif isinstance(d, list):
-        return [normalize_dict(x) for x in sorted(d, key=str) if x is not None]
-    else:
-        return d
-
-
 def test_config_dump(coco_dataset: LuxonisDataset):
     model_config_path = "configs/detection_light_model.yaml"
     temp_config_path = "tests/integration/test_saved_config.yaml"
@@ -32,8 +21,8 @@ def test_config_dump(coco_dataset: LuxonisDataset):
         model1 = LuxonisModel(cfg=temp_config_path, opts=opts)
         model2 = LuxonisModel(cfg=model_config_path, opts=opts)
 
-        model1_config = normalize_dict(model1.cfg.model_dump())
-        model2_config = normalize_dict(model2.cfg.model_dump())
+        model1_config = model1.cfg.dict()
+        model2_config = model2.cfg.dict()
 
         assert model1_config == model2_config, "Model configs are not the same"
         assert "Normalize" not in [
