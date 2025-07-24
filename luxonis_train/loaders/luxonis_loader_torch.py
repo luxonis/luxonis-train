ac0475a91d6fb4a7d140e7ef77e99c2e7517c70b
Martin Kozlovský <martin.kozlovsky@luxonis.com>
2025-07-24 15:47:20 +0200

    Option to download weights from s3 or gcs (#267)

--- luxonis_train/loaders/luxonis_loader_torch.py
+++ luxonis_train/loaders/luxonis_loader_torch.py
@@ -158,9 +158,9 @@ class LuxonisLoaderTorch(BaseLoaderTorch):
     @override
     def augment_test_image(self, img: dict[str, Tensor]) -> Tensor:
         if self.loader.augmentations is None:
-            return img
-        img = {k: v.numpy() for k, v in img.items()}
-        augmented_dict = self.loader.augmentations.apply([(img, {})])[0]
+            return img[self.image_source]
+        img_arr = {k: v.numpy() for k, v in img.items()}
+        augmented_dict = self.loader.augmentations.apply([(img_arr, {})])[0]
         return torch.tensor(next(iter(augmented_dict.values())))
 
     def _parse_dataset(
