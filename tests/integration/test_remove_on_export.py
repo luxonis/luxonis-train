4be5cb9460a1c70d9ac5e818d97309d24e93fe03
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-07-31 23:44:39 +0200

    fix test remove on export

--- tests/integration/test_remove_on_export.py
+++ tests/integration/test_remove_on_export.py
@@ -1,25 +1,14 @@
-from pathlib import Path
-
 import onnxruntime as rt
-import pytest
 from luxonis_ml.data import LuxonisDataset
 from luxonis_ml.typing import Params
 
 from luxonis_train.core import LuxonisModel
 
-ONNX_PATH = Path("tests/integration/ddrnet_segmentation.onnx")
 
+def test_train_only_heads(coco_dataset: LuxonisDataset, opts: Params):
+    config_file = "configs/segmentation_light_model.yaml"
 
-@pytest.fixture(autouse=True)
-def clear_files():
-    yield
-    ONNX_PATH.unlink(missing_ok=True)
-
-
-def test_train_only_heads(coco_dataset: LuxonisDataset):
-    config_file = "tests/configs/ddrnet.yaml"
-
-    opts: Params = {"loader.params.dataset_name": coco_dataset.dataset_name}
+    opts |= {"loader.params.dataset_name": coco_dataset.dataset_name}
 
     model = LuxonisModel(config_file, opts)
     results = model.test()
@@ -27,9 +16,11 @@ def test_train_only_heads(coco_dataset: LuxonisDataset):
     name_to_check = "aux_segmentation_head"
     is_in_results = any(name_to_check in key for key in results)
 
-    model.export(str(ONNX_PATH.parent))
+    model.export()
 
-    sess = rt.InferenceSession(str(ONNX_PATH))
+    sess = rt.InferenceSession(
+        model.run_save_dir / "export" / "segmentation_light.onnx"
+    )
     onnx_output_names = [output.name for output in sess.get_outputs()]
     is_in_output_names = any(
         name_to_check in name for name in onnx_output_names
