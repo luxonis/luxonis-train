e3b83ffc7f584ca18986a2029a00ebe5ea8e5fdc
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-08-02 00:28:59 +0200

    removed infer test

--- tests/integration/test_core.py
+++ tests/integration/test_core.py
@@ -2,12 +2,9 @@ import sqlite3
 import sys
 from pathlib import Path
 
-import cv2
-import numpy as np
 import pytest
-from luxonis_ml.data import LuxonisDataset, LuxonisLoader
+from luxonis_ml.data import LuxonisDataset
 from luxonis_ml.typing import Params
-from pytest_subtests import SubTests
 
 from luxonis_train.core import LuxonisModel
 
@@ -43,51 +40,6 @@ def test_tune(opts: Params, coco_dataset: LuxonisDataset, tempdir: Path):
     # Should be 4 * 6 = 24, but the augmentation
     # subset parameters are not stored in the database
     assert len(cur.fetchall()) == 20
-
-
-def test_infer(
-    coco_dataset: LuxonisDataset,
-    tempdir: Path,
-    image_size: tuple[int, int],
-    subtests: SubTests,
-):
-    loader = LuxonisLoader(coco_dataset)
-    img_dir = tempdir / "images"
-    save_dir = tempdir / "infer_results"
-    video_path = tempdir / "video.avi"
-    video_writer = cv2.VideoWriter(
-        str(video_path), cv2.VideoWriter_fourcc(*"XVID"), 1, (256, 256)
-    )
-    img_dir.mkdir()
-    for i, (img, _) in enumerate(loader):
-        assert isinstance(img, np.ndarray)
-        img = cv2.resize(img, (256, 256))
-        cv2.imwrite(str(img_dir / f"{i}.jpg"), img)
-        video_writer.write(img)
-    video_writer.release()
-
-    opts = {
-        "loader.params.dataset_name": coco_dataset.identifier,
-        "trainer.preprocessing.train_image_size": image_size,
-        "trainer.preprocessing.augmentations": [],
-    }
-    model = LuxonisModel("configs/complex_model.yaml", opts)
-
-    for subtest in ["single_image", "image_dir", "video", "loader"]:
-        with subtests.test(subtest):
-            save_dir = tempdir / f"infer_{subtest}"
-            if subtest == "single_image":
-                source = img_dir / "0.jpg"
-            elif subtest == "image_dir":
-                source = img_dir
-            elif subtest == "video":
-                source = video_path
-            else:
-                source = None
-            model.infer(source_path=source, save_dir=save_dir)
-
-    with pytest.raises(ValueError, match="is not a valid file or directory"):
-        model.infer(source_path="tests/data/invalid.jpg", save_dir=save_dir)
 
 
 def test_weights_loading(cifar10_dataset: LuxonisDataset, opts: Params):
