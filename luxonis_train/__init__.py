4c6351e178e1e134998987e73ebd677786b6f3c7
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-11-17 15:06:22 +0100

    bump version

--- luxonis_train/__init__.py
+++ luxonis_train/__init__.py
@@ -3,7 +3,7 @@ from typing import Final
 
 from pydantic_extra_types.semantic_version import SemanticVersion
 
-__version__: Final[str] = "0.4.0"
+__version__: Final[str] = "0.4.1"
 __semver__: Final[SemanticVersion] = SemanticVersion.parse(__version__)
 
 
