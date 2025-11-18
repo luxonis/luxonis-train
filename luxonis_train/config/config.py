c7842c51268b0d84030094feaac5b9e3a142cb98
klemen1999 <klemenskrlj8@gmail.com>
2025-11-18 10:18:26 +0100

    Added cast of str to int for GradAccScheduler

--- luxonis_train/config/config.py
+++ luxonis_train/config/config.py
@@ -1,4 +1,5 @@
 import sys
+from collections.abc import Mapping
 from contextlib import suppress
 from pathlib import Path
 from typing import Annotated, Any, Literal, NamedTuple
@@ -542,6 +543,30 @@ class TrainerConfig(BaseModelExtraForbid):
                     f"Number of epochs: {self.epochs}"
                 )
 
+        return self
+
+    @model_validator(mode="after")
+    def validate_gradient_acc_scheduler(self) -> Self:
+        """Keys in the GradientAccumulationSheduler.params.scheduling
+        should be ints but yaml can sometime auto-convert them to
+        strings.
+
+        This converts them back to ints if possible.
+        """
+        for callback in self.callbacks:
+            if callback.name != "GradientAccumulationScheduler":
+                continue
+
+            scheduling = callback.params.get("scheduling")
+            if not isinstance(scheduling, Mapping):
+                # Continue from Config verification standpoint but it might
+                # fail due to GradientAccumulationScheduler param verification
+                continue
+
+            callback.params["scheduling"] = {
+                int(k) if isinstance(k, str) and k.isdigit() else k: v
+                for k, v in scheduling.items()
+            }
         return self
 
     @model_validator(mode="after")
