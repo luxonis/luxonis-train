c183a8438d6d629dcf4da8d097a7456f796a4cad
Dani Rogmans <danirogmans@gmail.com>
2026-01-09 15:38:00 +0100

    pre-commit

--- luxonis_train/callbacks/convert_on_train_end.py
+++ luxonis_train/callbacks/convert_on_train_end.py
@@ -2,14 +2,13 @@ import lightning.pytorch as pl
 from loguru import logger
 
 import luxonis_train as lxt
-from luxonis_train.registry import CALLBACKS
 
 from .needs_checkpoint import NeedsCheckpoint
 
 
 class ConvertOnTrainEnd(NeedsCheckpoint):
-    """Callback that exports, archives, and converts the model on train end.
-    """
+    """Callback that exports, archives, and converts the model on train
+    end."""
 
     def on_train_end(
         self, _: pl.Trainer, pl_module: "lxt.LuxonisLightningModule"
